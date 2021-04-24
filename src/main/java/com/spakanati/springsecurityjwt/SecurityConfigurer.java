e7a511f76614e84a5ace953b10d77422ad55858e
srinivas931 <srinivas.pakanati@envestnet.com>
2021-04-24 16:43:41 -0400

    Spring app intial commit

---
+++ src/main/java/com/spakanati/springsecurityjwt/SecurityConfigurer.java
@@ -0,0 +1,17 @@
+package com.spakanati.springsecurityjwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyUserDetailsService myUserDetailService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailService);
+    }
+}
